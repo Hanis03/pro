73264464e75ee415d74157c42d879043d03b9040
Jan Vítek <jan.vitek@student.spsmb.cz>
2023-10-05 11:11:14 +0200

    Opakování

--- pro_ukol_5_10_2023/opakování/src/Main.java
+++ pro_ukol_5_10_2023/opakování/src/Main.java
@@ -6,30 +6,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int input;
-
         do {
             System.out.print(" 1 - Zjištění přestupného roku,\n 2 - Nejbližší přestupný rok,\n 3 - Přepsání z ms na čas,\n 4 - Vypočítání rozdílu od aktuálního data,\n 5 - Konec\n Vyberte úkol: ");
             input = scanner.nextInt();
-
             switch (input) {
                 case 1:
-                    Ukol1 ukol1 = new Ukol1();
-                    ukol1.spustit();
+                    new Ukol1().spustit();
                     break;
                 case 2:
-                    Ukol2 ukol2 = new Ukol2();
-                    ukol2.spustit();
+                    new Ukol2().spustit();
                     break;
                 case 3:
-                    Ukol3 ukol3 = new Ukol3();
-                    ukol3.spustit();
+                    new Ukol3().spustit();;
                     break;
                 case 4:
-                    Ukol4 ukol4 = new Ukol4();
-                    ukol4.spustit();
+                    new Ukol4().spustit();
                     break;
                 case 5:
-                    System.out.println("Končím..");
+                    System.out.println("");System.out.println("Končím..");
                     break;
                 default:
                     System.out.println("Neplatné číslo operace.");
@@ -57,14 +51,14 @@ class Ukol1 {
 class Ukol2 {
     public void spustit() {
         int year = Year.now().getValue();
-        if (Year.isLeap(year)) {
-            System.out.println("Tento rok je přestupný.");
-            System.out.println("");
-        } else {
-            Year nextLeapYear = Year.of(year).plusYears(1);
-            System.out.println("Přestupný rok je: " + nextLeapYear);
-            System.out.println("");
+        while (true) {
+            if (Year.isLeap(year)) {
+                System.out.println("Přestupný rok je: " + year);
+                break;
+            }
+            year++;
         }
+        System.out.println("");
     }
 }
 
@@ -88,12 +82,23 @@ class Ukol4 {
         LocalDate zadaneDatum = LocalDate.of(rok, Month.of(mesic), den);
         LocalDate dnes = LocalDate.now();
         LocalTime casNyni = LocalTime.now();
-        LocalDate aktualniDatum = dnes.isBefore(zadaneDatum) ? zadaneDatum : dnes;
-        Period period = Period.between(dnes, zadaneDatum);
-        Duration duration = Duration.between(casNyni, LocalTime.MIDNIGHT);
-        System.out.println("Zbývá " + period.getYears() + " let, " + period.getMonths() + " měsíců, "
-                + period.getDays() + " dní, " + duration.toHours() + " hodin, " + duration.toMinutes()
-                + " minut a " + duration.getSeconds() + " sekund do zadaného data.");
+        LocalDateTime aktualniCasDatum = LocalDateTime.of(dnes, casNyni);
+        LocalDateTime zadaneCasDatum = LocalDateTime.of(zadaneDatum, LocalTime.MIDNIGHT);
+        Duration duration = Duration.between(aktualniCasDatum, zadaneCasDatum);
+        long celkoveSekundy = duration.getSeconds();
+        long sekundy = celkoveSekundy % 60;
+        long minuty = (celkoveSekundy / 60) % 60;
+        long hodiny = (celkoveSekundy / 3600) % 24;
+        long roky = (long) ((celkoveSekundy / 31536000 ) % 365.25);
+        long dny = (long) ((celkoveSekundy / 86400) - roky * 365.25);
+
+        System.out.println("Zbývají "+ roky + " rok(y)/ let, " + dny + " dní, " + hodiny + " hodin, " + minuty + " minut a " + sekundy + " sekund do zadaného data.");
         System.out.println("");
     }
 }
+
+
+
+
+
+
